1154a5fd7566c095dfce9d306adf96fb6bc636c5
viiindesu <vincent.535240039@stu.untar.ac.id>
2025-04-11 17:44:05 +0700

    sedikit perubahan dg logic animes-schema

---
+++ src/models/animes-schema.js
@@ -0,0 +1,58 @@
+/* eslint-disable prettier/prettier */
+module.exports = (db) => {
+  db.Model(
+    'Anime',
+    db.Schema({
+      title_en: String,
+      title_jp: String,
+      episodes: Number,
+      studio: String,
+      status: {
+        type: String,
+        enum: ['airing', 'completed', 'upcoming'],
+        required: true,
+      },
+      season: {
+        type: String,
+        enum: ['Winter', 'Spring', 'Summer', 'Fall'],
+        required: true,
+      },
+      airing_date: {
+        start: {
+          type: Date, // null if the airing date is TBA
+          required: false,
+        },
+        end: {
+          type: Date, // null if the anime is still airing
+          required: false,
+        },
+      },
+      age_rating: {
+        type: String,
+        enum: ['G', 'PG', 'PG-13', 'R-17+'],
+        required: true,
+      },
+      demographics: {
+        type: String,
+        enum: ['shounen', 'shoujo', 'seinen', 'josei', 'kodomo'],
+        required: true,
+      },
+      genres: {
+        type: [String],
+        enum: [
+          'action',
+          'adventure',
+          'comedy',
+          'drama',
+          'fantasy',
+          'horror',
+          'mystery',
+          'romance',
+          'sci-fi',
+          'slice of life',
+        ],
+        required: true,
+      },
+    })
+  );
+};
